855cccc83dcba510cc09f3b4906a3c298cf68b41
RehmaNadeem <rehmanadeem2424@gmail.com>
2025-01-31 01:26:05 +0500

    Add files via upload

---
+++ Assignment_No_11/hello.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello My World!")
+
+
+if __name__ == "__main__":
+    main()
+    print("Hello from project-uv!")
---
+++ Assignment_No_11/src/assignment_no_11/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from assignment-no-11!")
